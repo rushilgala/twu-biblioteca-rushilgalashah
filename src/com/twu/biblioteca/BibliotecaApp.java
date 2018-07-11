f826b6298e8243a63026c4610f4b9afd07d4de7a
Rushil Gala-Shah <rushil@gala-shah.co.uk>
2018-07-11 14:55:19 +0100

    Implemented welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        System.out.println(bibliotecaApp.welcomeMessage());
+    }
+
+    public String welcomeMessage() {
+        return "Welcome to Biblioteca!";
     }
 }
