e0638de40181c6e24ec43f5fd7491f62915da050
Rushil Gala-Shah <rgalashah@thoughtworks.com>
2018-07-19 16:11:08 +0100

    User State implemented

--- src/com/twu/biblioteca/UserState.java
+++ src/com/twu/biblioteca/UserState.java
@@ -1,5 +1,70 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class UserState {
 
+	private static User user;
+
+	public UserState() {
+		user = null;
+	}
+
+	public static String executeCommand(String input, User[] users) {
+		if (input.equals("S")) {
+			processSignIn(users);
+			return generateMessage();
+		} else {
+			return null;
+		}
+	}
+
+	public static void processSignIn(User[] users) {
+		Scanner sc = new Scanner(System.in);
+		String libraryNumber = getInput(sc,"Enter library number: ");
+		String password = getInput(sc,"Enter password: ");
+		setLoggedInUser(signIn(libraryNumber,password,users));
+	}
+
+	private static String generateMessage() {
+		if (getLoggedInUser() != null) {
+			return new StringBuilder().append("Welcome ")
+					.append(getLoggedInUser().getName())
+					.append(", you are now signed in.")
+					.toString();
+		} else {
+			return "Invalid card number or password.";
+		}
+	}
+
+	private static String getInput(Scanner sc,String prompt) {
+		System.out.print(prompt);
+		return sc.nextLine();
+	}
+
+	public static User signIn(String libraryNumber, String password, User[] users) {
+		for (User user : users) {
+			if (checkCredentials(libraryNumber, password, user)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public static User getLoggedInUser() {
+		return user;
+	}
+
+	public static boolean isLoggedIn() {
+		return user != null;
+	}
+
+	public static void setLoggedInUser(User newUser) {
+		user = newUser;
+	}
+
+	public static boolean checkCredentials(String libraryNumber, String password, User user) {
+		return user.getLibraryNumber().equals(libraryNumber) &&
+				user.getPassword().equals(password);
+	}
 }
